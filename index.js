456cf346575dd9db45eff72418fe3badc0f7d7ee
Jeremiah Harlan <jeremiah.harlan@gmail.com>
2015-05-21 15:56:37 -0400

    Make slack-irc modular so we can require it instead of passing in a file

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
-#!/usr/bin/env node
+module.exports = function(config) {
+  if (!config) throw new Error('You must specify initilization configuration');
 
-var program = require('commander');
-var checkEnv = require('check-env');
-var createBots = require('./lib/helpers').createBots;
-var logger = require('winston');
-
-if (process.env.NODE_ENV === 'development') {
-  logger.level = 'debug';
-}
-
-program
-  .version('1.1.0')
-  .option('-c, --config <path>',
-    'Sets the path to the config file, otherwise read from the env variable CONFIG_FILE.'
-  )
-  .parse(process.argv);
-
-// If no config option is given, try to use the env variable:
-if (!program.config) checkEnv(['CONFIG_FILE']);
-else process.env.CONFIG_FILE = program.config;
-
-createBots();
+  return require('./lib/helpers').createBot(config);
+};
--- test/create-bots.test.js
+++ test/create-bots.test.js
@@ -10,44 +10,48 @@ chai.should();
 chai.use(sinonChai);
 
 describe('Create Bots', function() {
+
+  var config;
+
   before(function() {
     this.connectStub = sinon.stub();
     Bot.prototype.connect = this.connectStub;
-    process.env.CONFIG_FILE = process.cwd() + '/test/fixtures/test-config.json';
+    config = require(process.cwd() + '/test/fixtures/test-config.json');
   });
 
   afterEach(function() {
+    config = null;
     this.connectStub.reset();
   });
 
   it('should work when given an array of configs', function() {
-    process.env.CONFIG_FILE = process.cwd() + '/test/fixtures/test-config.json';
-    var bots = createBots();
+    config = require(process.cwd() + '/test/fixtures/test-config.json');
+    var bots = createBots(config);
     bots.length.should.equal(2);
     this.connectStub.should.have.been.called;
   });
 
   it('should work when given an object as a config file', function() {
-    process.env.CONFIG_FILE = process.cwd() + '/test/fixtures/single-test-config.json';
-    var bots = createBots();
+    config = require(process.cwd() + '/test/fixtures/single-test-config.json');
+    var bots = createBots(config);
     bots.length.should.equal(1);
     this.connectStub.should.have.been.called;
   });
 
   it('should throw a configuration error if any fields are missing', function() {
-    process.env.CONFIG_FILE = process.cwd() + '/test/fixtures/bad-config.json';
+    config = require(process.cwd() + '/test/fixtures/bad-config.json');
     function wrap() {
-      createBots();
+      createBots(config);
     }
 
     (wrap).should.throw(ConfigurationError, 'Missing configuration field nickname');
   });
 
   it('should throw if a configuration file is neither an object or an array', function() {
-    process.env.CONFIG_FILE = process.cwd() + '/test/fixtures/string-config.json';
+    config = require(process.cwd() + '/test/fixtures/string-config.json');
 
     function wrap() {
-      createBots();
+      createBots(config);
     }
 
     (wrap).should.throw(ConfigurationError);
